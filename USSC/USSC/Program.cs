301a1d7a952f99125745bb8bba284fc4d95913a9
Kostya Golunov <kostya.golunov2015@yandex.ru>
2022-11-07 12:37:05 +0500

    Мелкие правки для корректной работы авторизации. Пока тестово была подключена бд Postgres

--- USSC/USSC/Program.cs
+++ USSC/USSC/Program.cs
@@ -1,5 +1,10 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using USSC;
 using USSC.Dto;
+using USSC.Helpers;
+using USSC.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +14,26 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddDbContext<DataContext>(opt => 
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped(typeof(IEfRepository<>), typeof(UserRepository<>));
+builder.Services.AddScoped<IUserService, UserService>();
+
+builder.Services.AddAutoMapper(typeof(UserProfile));
+builder.Services.AddCors();
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Sailora WEB API API", Version = "v1" });
+});
 //builder.Services.AddDbContext<ModelsContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,10 +42,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(x => x
+    .AllowAnyOrigin()
+    .AllowAnyMethod()
+    .AllowAnyHeader());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseMiddleware<JwtMiddleware>();
+// app.UseEndpoints(x => x.MapControllers());
 
 app.Run();
