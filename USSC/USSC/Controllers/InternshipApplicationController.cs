88452b9a1653d350b365800e09ba839e29febeb5
Kostya Golunov <kostya.golunov2015@yandex.ru>
2022-11-09 00:07:36 +0500

    Создал пустой контроллер для обработки заявки (создание и получения заявок)

---
+++ USSC/USSC/Controllers/InternshipApplicationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace USSC.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class InternshipApplicationController : ControllerBase
+{
+    [HttpPost("createApplication")]
+    public IActionResult CreateApplication()
+    {
+        // сделать проверку на авторизацию
+        // if ()
+        return Ok();
+    }
+
+    [HttpGet("GetApplication")]
+    public IActionResult GetApplication()
+    {
+        // проверка на доступ пользователя к заявкам role == Tutor || Admin
+        // if ()
+        return Ok();
+    }
+}
