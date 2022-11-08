ac3b05ebca6dcf62b06c84aa7b30b322c94a208a
Kostya Golunov <kostya.golunov2015@yandex.ru>
2022-11-08 23:35:23 +0500

    Создал пустой контроллер для загрузки и выгрузки файла тестового задания

---
+++ USSC/USSC/Controllers/TestCaseController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace USSC.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestCaseController : ControllerBase
+{
+    [HttpGet("downoload")]
+    public IActionResult DownoloadFile()
+    {
+        // сделать проверку на авторизацию и доступ к практике
+        // if ()
+        return Ok();
+    }
+
+    [HttpPost("load")]
+    public IActionResult LoadFile()
+    {
+        // сделать проверку на авторизацию и доступ к практике
+        // можно вынести в отдельный метод эту проверку
+        // if ()
+        return Ok();
+    }
+}
