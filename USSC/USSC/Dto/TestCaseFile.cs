f765d9b0c816054469965dee216ff838c3dd6ead
Kostya Golunov <kostya.golunov2015@yandex.ru>
2022-11-17 12:24:02 +0500

    Прописал основные dto, без связей

---
+++ USSC/USSC/Dto/TestCaseFile.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace USSC.Dto;
+
+public class TestCaseFile
+{
+    [Key]
+    public long Id { get; set; }
+    public long PracticesId { get; set; }
+    public string? Path { get; set; }
+}
