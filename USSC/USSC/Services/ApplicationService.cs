394d69b7024a62f25c477026ea34b73d797354d5
Danil <donul.mukhametshin.03@gmail.com>
2022-11-19 01:44:43 +0500

    Application Logic

---
+++ USSC/USSC/Services/ApplicationService.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
+using USSC.Dto;
+using USSC.Entities;
+
+namespace USSC.Services;
+
+public class ApplicationService: IApplicationService, IDisposable
+{
+    private readonly IEfRepository<Application> _applicationRepository;
+    private readonly IConfiguration _configuration;
+    private readonly IMapper _mapper;
+    
+    public ApplicationService(IEfRepository<Application> applicationRepository, IConfiguration configuration, IMapper mapper)
+    {
+        _applicationRepository = applicationRepository;
+        _configuration = configuration;
+        _mapper = mapper;
+    }
+    
+    public IEnumerable<Application> GetAll() => _applicationRepository.GetAll();
+
+    public Application GetById(int id) => _applicationRepository.GetById(id);
+
+    public async Task<IActionResult> SubmitApplicationAsync(ApplicationModel applicationModel)
+    {
+        var application = _mapper.Map<Application>(applicationModel);
+
+        await _applicationRepository.Add(application);
+
+        return new OkResult();
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+    }
+}
