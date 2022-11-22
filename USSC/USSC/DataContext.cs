78e27f8d8475e994a7f8438d32a59400206cec28
Kostya Golunov <kostya.golunov2015@yandex.ru>
2022-11-22 19:40:08 +0500

    Мелкие правки для корректной работы авторизации

--- USSC/USSC/DataContext.cs
+++ USSC/USSC/DataContext.cs
@@ -7,14 +7,14 @@ namespace USSC;
 
 public class DataContext : DbContext
 {
-    public DbSet<UsersEntity> Users { get; set; }/*
+    public DbSet<UsersEntity> Users { get; set; }
     public DbSet<ApplicationDirectionsfkEntity> ApplicationDirectionsfk { get; set; }
     public DbSet<ApplicationEntity> Application { get; set; }
     public DbSet<DirectionsEntity> Directions { get; set; }
     public DbSet<PracticesEntity> Practices { get; set; }
     public DbSet<TeachersEntity> Teachers { get; set; }
     public DbSet<UsersPracticesfkEntity> UsersPracticesfk { get; set; }
-    public DbSet<TestCaseEntity> TestCase { get; set; }*/
+    public DbSet<TestCaseEntity> TestCase { get; set; }
 
     public DataContext(DbContextOptions<DataContext> options): base(options)
     {
