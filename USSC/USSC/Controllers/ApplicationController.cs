bb1203566367e4ce55a8d04e2ce9b907fe2ae2eb
Kostya Golunov <kostya.golunov2015@yandex.ru>
2022-12-11 04:03:32 +0500

    Попытка сделать рабочим email отправку

--- USSC/USSC/Controllers/ApplicationController.cs
+++ USSC/USSC/Controllers/ApplicationController.cs
@@ -42,6 +42,8 @@ public class ApplicationController : ControllerBase
     public async Task<IActionResult> ProcessApplication(RequestModel requestModel)
     {
         var response = await _applicationService.ProcessRequest(requestModel);
+        var emailSender = new EmailSender();
+        emailSender.SendEmail("Ваша заявка проверена", "kostya.golunov2015@yandex.ru");
         return Ok(response);
     }
     
--- USSC/USSC/Helpers/EmailSender.cs
+++ USSC/USSC/Helpers/EmailSender.cs
@@ -8,15 +8,15 @@ public class EmailSender
     public void SendEmail(string message, string mailTo)
     {
         var Mailmessage = new MimeMessage();
-        Mailmessage.From.Add(new MailboxAddress("Моя компания", "admin@mycompany.com")); //отправитель сообщения поменять адрес
+        Mailmessage.From.Add(new MailboxAddress("Моя компания", "teampincode@mail.ru")); //отправитель сообщения поменять адрес
         Mailmessage.To.Add(new MailboxAddress("Моя компания",mailTo)); //адресат сообщения поменять адрес
         Mailmessage.Subject = "Сообщение от USummerSchool"; //тема сообщения 
         Mailmessage.Body = new BodyBuilder() { HtmlBody = $"<div style=\"color: orange;\">{message}</div>" }.ToMessageBody(); //тело сообщения (так же в формате HTML)
 
         using (SmtpClient client = new SmtpClient())
         {
-            client.Connect("smtp.gmail.com", 587, true); //либо использум порт 465
-            client.Authenticate("mail@gmail.com", "secret"); //логин-пароль от аккаунта
+            client.Connect("smtp.mail.ru", 465, true); //либо использум порт 465
+            client.Authenticate("teampincode@mail.ru", "sh3RnqZP"); //логин-пароль от аккаунта
             client.Send(Mailmessage);
 
             client.Disconnect(true);
