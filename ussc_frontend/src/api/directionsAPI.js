82df95f5f89711b288f8ec523f377cba20830be1
roma-cheby <105049918+roma-cheby@users.noreply.github.com>
2022-12-21 09:34:44 +0500

    Update directionsAPI.js

--- ussc_frontend/src/api/directionsAPI.js
+++ ussc_frontend/src/api/directionsAPI.js
@@ -1,8 +1,8 @@
 import { HOST } from './host';
 
-export const GET_DIRECTIONS_URL = `${HOST}/practices/GetPractices`;
-export const UPDATE_DIRECTION_URL = `${HOST}/practices/UpdatePractices`;
-export const CREATE_DIRECTION_URL = `${HOST}/practices/CreatePractices`;
+export const GET_DIRECTIONS_URL = `${HOST}/practices/get`;
+export const UPDATE_DIRECTION_URL = `${HOST}/practices/update`;
+export const CREATE_DIRECTION_URL = `${HOST}/practices/create`;
 
 const DIRECTIONS_API = {
   GET_DIRECTIONS_URL,
